728567d9319487794a2cbcf8746877b2fb73d5ea
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2022-10-13 17:17:03 +0200

    Adding a dynamic router for every lodgement

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route  path = "/" element={<Homepage/>} />
-          <Route  path = "/logements" element={<Lodgement/>} />
+          <Route  path = "/logements/:id" element={<Lodgement/>} />
           <Route  path = "/apropos" element={<About/>} />
           <Route  path = "/*" element={<Error/>} />
         </Routes>
--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Data } from "../../Data";
 import backgroundHomePage from "./background_homepage.png";
 import "./__Homepage.scss";
@@ -6,19 +7,35 @@ import "./__Homepage.scss";
 export default function Homepage() {
   return (
     <div className="homepage">
-    <div className="background">
-    <img src={backgroundHomePage} alt="" className="background__image"/>
-    <h1 className="background__title">Chez vous et partout ailleurs </h1>
-    </div>
-    <div className="lodgement__container">
-      {Data.map((postDetail, index) => {
-        return (
+      <div className="background">
+        <img src={backgroundHomePage} alt="" className="background__image" />
+        <h1 className="background__title">Chez vous et partout ailleurs </h1>
+      </div>
+      <div className="lodgementContainer">
+        {Data.map((postDetail, index) => {
+          return (
             <>
-              <span>{postDetail.title}</span>;
-              <img src={postDetail.cover} alt= {postDetail.title} className="lodge__cover" />
+          
+              <Link to =  {`/logements/:${postDetail.id}`} >
+              <article className="box">
+                  <img
+                    src={postDetail.cover}
+                    alt={postDetail.title}
+                    className="box__cover"
+                  />
+                  <h2 className="box__title">
+                    {postDetail.title}
+                  </h2>
+                  <div className="box__filter">
+                    
+                  </div>
+                  ;
+                </article>
+              </Link>
+              
             </>
-        );
-      })}
+          );
+        })}
       </div>
     </div>
   );
