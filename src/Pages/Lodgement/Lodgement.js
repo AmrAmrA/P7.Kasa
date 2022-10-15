ee5ff5fc13078bb709435125b80bf483b66268c2
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2022-10-15 22:37:22 +0200

    Approximat design without responsive

--- src/Pages/Lodgement/Lodgement.js
+++ src/Pages/Lodgement/Lodgement.js
@@ -1,40 +1,48 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { Data } from "../../Data";
+import Accordion from "../../Components/Accordion/Accordion";
 import "./__Lodgement.scss";
 import redStar from "./redStar.png";
 import greyStar from "./greyStar.png";
+
 export default function Lodgement() {
   const params = useParams();
 
   const starsArray = [1, 2, 3, 4, 5];
   return (
     <>
-      <p>{params.id}</p>
-      <div className="container">
+      <div className="container__lodgement">
         {Data.filter((card) => card.id === params.id).map((card, index) => (
           <div key={index} className="fullcard">
-            <h1>{card.title}</h1>
-            <h1>{card.location}</h1>
-            <p>{card.description}</p>
             <img
-              src={card.host.picture}
-              alt={`L'hôte de la maison: ${card.host.name}`}
+              src={card.cover}
+              alt="Un slider de plusieurs clichés pour présenter l'appartement"
+              className="image__lodgement"
             />
-            <p>{card.host.name}</p>
-            <ul>
-              {card.equipments.map((cardItem, index) => (
-                <li>{cardItem} </li>
-              ))}
-              ;
-            </ul>
-            <ul>
+
+            <div className="lodgementDescription">
+              <div className="geographicInofrmations">
+                <h1>{card.title}</h1>
+                <p>{card.location}</p>
+              </div>
+              <div className="hostInformations">
+                <p>{card.host.name}</p>
+                <img
+                  src={card.host.picture}
+                  alt={`L'hôte de la maison: ${card.host.name}`}
+                />
+              </div>
+            </div>
+
+            <div className="lodgementInformations">
+            <ul className="tagsList">
               {card.tags.map((tagsItem, index) => (
-                <li> {tagsItem} </li>
+                <li className="tagsItems"> {tagsItem} </li>
               ))}
-              ;
             </ul>
-            <div>
+
+            <div className="starsList">
               {starsArray.map((index) => (
                 <img
                   alt="Nombre d'étoiles pour évaluer la qualité du logement"
@@ -44,6 +52,20 @@ export default function Lodgement() {
                 />
               ))}
             </div>
+            </div>
+
+            <div className="accordions">
+              <Accordion title="Description" content={card.description} />
+              <Accordion
+                title="Équipements"
+                content=<ul>
+                  {card.equipments.map((cardItem, index) => (
+                    <li>{cardItem} </li>
+                  ))}
+                </ul>
+              />
+            </div>
+            <p></p>
           </div>
         ))}
       </div>
