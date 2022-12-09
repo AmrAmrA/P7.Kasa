ba04eceee6aca58fed7bb8313d6c74027a07d104
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2022-12-09 16:35:21 +0100

    Add a new component for lodgement informations

---
+++ src/Components/LodgementLocation/LodgementLocation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import LodgmentData from "../../logements";
+import "./__LodgementLocation.scss";
+
+export default function LodgementLocation() {
+  const params = useParams();
+  return (
+    <>
+      {LodgmentData.filter((card) => card.id === params.id).map(
+        (card, index) => (
+          <>
+            <h1>{card.title}</h1>
+            <p>{card.location}</p>
+          </>
+        )
+      )}
+      
+    </>
+  );
+}
--- src/Pages/Lodgement/Lodgement.js
+++ src/Pages/Lodgement/Lodgement.js
@@ -6,6 +6,7 @@ import Slider from "../../Components/Slider/Slider";
 import StarsRating from "../../Components/StarsRating/StarsRating";
 import Tags from "../../Components/Tags/Tags";
 import Host from "../../Components/Host/Host";
+import LodgementLocation from "../../Components/LodgementLocation/LodgementLocation";
 import Error from "../../Pages/Error/Error";
 import "./__Lodgement.scss";
 
@@ -13,30 +14,30 @@ export default function Lodgement() {
   // A hook who allows us to display data for every differnt ID inside the JSON File
   const params = useParams();
   const index = LodgmentData.findIndex((el) => el.id === params.id);
-  if (index === -1) return <Error />;
+  if (index === -1) return <Error/>;
 
   return (
     <main className="container__lodgement">
-      <Slider />
+      <Slider/>
       {LodgmentData.filter((card) => card.id === params.id).map(
         (card, index) => (
           <section key={index} className="fullcard">
             <article className="lodgementDescription">
               <div className="geographicInofrmations">
-                <h1>{card.title}</h1>
-                <p>{card.location}</p>
+                <LodgementLocation/>
                 <Tags />
               </div>
               <div className="hostInformations">
                 <Host />
-                <StarsRating />
+                <StarsRating/>
               </div>
             </article>
             <div className="accordions">
               <Accordion title="Description" content={card.description} />
               <Accordion
                 title="Équipements"
-                content=<ul>
+                content
+                  =<ul>
                   {card.equipments.map((cardItem, index) => (
                     <li>{cardItem} </li>
                   ))}
